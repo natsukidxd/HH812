5916eb321f1de71b74fe55f3139386de93b531d7
natsukidxd <natsukid123@gmail.com>
2026-03-30 13:43:40 +0800

    fast commit

--- schooldb/dbhelper.py
+++ schooldb/dbhelper.py
@@ -2,8 +2,7 @@ from sqlite3 import connect,Row
 
 database:str = "school.db"
 
-def getall(sql:str,vals:list)->list:
-    print(sql)
+def getprocess(sql:str,vals:list)->list:
     conn:any = None
     cursor:any = None
     data:list = []
@@ -11,7 +10,7 @@ def getall(sql:str,vals:list)->list:
         conn = connect(database)
         conn.row_factory = Row
         cursor = conn.cursor()
-        cursor.execute(sql,vals)
+        cursor.execute(sql,vals)        
         data=cursor.fetchall()
     except Exception as ex:
         print(f"Error : {ex}")
@@ -20,15 +19,65 @@ def getall(sql:str,vals:list)->list:
         conn.close()
     return data
 
-def getrecord(idno:str)->list:
-    sql:str = "SELECT * FROM `students` WHERE `idno`=?"
-    return getall(sql,[idno])
+def getall(table:str)->list:
+    sql:str = f"SELECT * FROM `{table}`"
+    return getprocess(sql,[])
+    
+def getrecord(table:str,**kwargs)->list:
+    keys:list = list(kwargs.keys())
+    vals:list = list(kwargs.values())
+    sql:str = f"SELECT * FROM `{table}` WHERE `{keys[0]}`=?"
+    return getprocess(sql,vals)
 
+def postprocess(sql:str,vals:list)->bool:
+    print(sql)
+    conn:any = None
+    cursor:any=None
+    try:
+        conn = connect(database)
+        cursor = conn.cursor()
+        cursor.execute(sql,vals)
+        conn.commit()        
+    except Exception as ex:
+        conn.rollback()
+        print(f"Error : {ex}")
+    finally:
+        cursor.close()
+        conn.close()
+    return True if cursor.rowcount>0 else False
+
+def addrecord(table:str,**kwargs)->bool:
+    keys:list = list(kwargs.keys())
+    vals:list = list(kwargs.values())
+    qmark:list = '?'*len(vals)
+    flds:str = '`,`'.join(keys)
+    dta:str = ','.join(qmark)
+    sql:str = f"INSERT INTO `{table}`(`{flds}`) VALUES ({dta})"
+    return postprocess(sql,vals)
+    
+def deleterecord(table:str,**kwargs)->bool:
+    keys:list = list(kwargs.keys())
+    vals:list = list(kwargs.values())
+    sql:str = f"DELETE FROM `{table}` WHERE `{keys[0]}`=?"
+    return postprocess(sql,vals)
+    
+def updaterecord(table:str,**kwargs)->bool:
+    #UPDATE `students` SET `lastname`=?,`firstname`=?,`course`=?,`level`=? WHERE `idno`='1000'
+    keys:list = list(kwargs.keys())
+    vals:list = list(kwargs.values())
+    flds:list = []
+    newvals:list = []
+    for i in range(1,len(keys)):
+        flds.append("`"+keys[i]+"`=?")
+        newvals.append(vals[i])
+    fld:str = ",".join(flds)
+    sql:str = f"UPDATE `{table}` SET {fld} WHERE `{keys[0]}`='{vals[0]}'"
+    return postprocess(sql,newvals)
+        
 def main()->None:
-    # sql:str = "SELECT * FROM `students`"
-    students:list = addrecord('1003', 'ferrer', 'krist dave', 'bsit', '3')
+    
+    students:list = getrecord('students',idno='1000')
     for student in students:
-        print(f"{student['idno']:<10} {student['lastname'].upper():<25} {student['firstname'].upper():<25} {student['course'].upper():<10} {student['level'].upper():<5}")
-
+        print(f"{student['lastname'].upper():<25} {student['firstname'].upper():<25}")
 if __name__=="__main__":
     main()
